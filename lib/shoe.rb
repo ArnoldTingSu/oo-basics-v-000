2f85853e8908c0703ed32dc1e83ea257077033db
Arnold Su <arnoldtingsu@gmail.com>
2018-11-21 22:31:42 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@
 
 class Shoe
   
-  attr_accessor :condition, :color, :size, :material :cobble
+  attr_accessor :condition, :color, :size, :material, :cobble
   attr_reader :brand
   
   def initialize(brand)
